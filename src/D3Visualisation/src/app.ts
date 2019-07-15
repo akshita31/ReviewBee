68876962b6332e1aaf889026d91f8e6829f124f7
akshita007 <akagarw@microsoft.com>
2019-07-14 23:18:30 -0700

    chart is rendering with d3.js

--- src/D3Visualisation/src/app.ts
+++ src/D3Visualisation/src/app.ts
@@ -1,3 +1,13 @@
 import * as d3 from 'd3';
- 
+
 d3.select('body').append("span");
+
+var data = [30, 86, 168, 281, 303, 365];
+
+d3.select(".chart")
+  .selectAll("div")
+  .data(data)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return d * 2 + "px"; })
+    .text(function(d) { return '$ ' + d; });
