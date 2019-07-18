141c6bf02be828e70400991f5781a55cb509e416
akshita007 <akagarw@microsoft.com>
2019-07-18 00:40:28 -0700

    use builalyzer

--- src/RoslynAPI/RoslynSemanticGraphGetter.cs
+++ src/RoslynAPI/RoslynSemanticGraphGetter.cs
@@ -1,14 +1,34 @@
-﻿using System;
-using Microsoft.CodeAnalysis;
-using System.IO;
+﻿using System.IO;
+using System.Linq;
+using Buildalyzer;
+using Buildalyzer.Workspaces;
 
 namespace RoslynAPI
 {
-    public class RoslynSemanticGraphGetter: ISemanticGraphGetter
+    public class RoslynSemanticGraphGetter : ISemanticGraphGetter
     {
-        public void GetSemanticGraph(DirectoryInfo path)
+        public void GetSemanticGraph(DirectoryInfo directory)
         {
+            //directory where solution
+            if (!directory.Exists)
+            {
+                throw new DirectoryNotFoundException($"Could not find directory {directory.FullName}");
+            }
 
+            var solutionFile = directory.EnumerateFiles("*.sln", SearchOption.AllDirectories).FirstOrDefault();
+            if (solutionFile != null)
+            {
+                var manager = new AnalyzerManager(solutionFile.FullName);
+                foreach (var prj in manager.Projects.Values)
+                {
+                    // GetWorkspace returns Microsoft.CodeAnalysis.AdhocWorkspace which can be used with Roslyn
+                    var workspace = prj.GetWorkspace();
+                    workspace.CurrentSolution.GetProjectDependencyGraph();
+                    var sln = workspace.CurrentSolution;
+
+                    //await AnalyzeProject(sln.Projects.First());
+                }
+            }
         }
     }
 }
