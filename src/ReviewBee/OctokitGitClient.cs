bec970e1c7f1e61a410724fbe9915e1dbfd33347
Akshita Agarwal <akagarw@microsoft.com>
2019-07-21 16:24:59 -0700

    Clean up and stroing the directory at a particular position

--- src/ReviewBee/OctokitGitClient.cs
+++ src/ReviewBee/OctokitGitClient.cs
@@ -20,31 +20,26 @@ namespace ReviewBee
 
         public async Task<IEnumerable<PullRequestFile>> GetPullRequestFiles(string owner, string name, int pullRequestNumber)
         {
-           
-            var prFiles = await _client.PullRequest.Files(owner, name,pullRequestNumber);
-           return prFiles.Select(file => new PullRequestFile(file.FileName, file.Patch));
+
+            var prFiles = await _client.PullRequest.Files(owner, name, pullRequestNumber);
+            return prFiles.Select(file => new PullRequestFile(file.FileName, file.Patch));
         }
 
-        public async Task DownloadGithubRepo(string owner, string name, string path,string commitId)
+        public async Task DownloadGithubRepo(string owner, string name, string path, string commitId, DirectoryInfo downloadDirectory)
         {
             var id = (await _client.Repository.Get(owner, name)).Id;
             IReadOnlyList<RepositoryContent> repoContent = await _client.Repository.Content.GetAllContentsByRef(id, path, commitId);
 
             var repoArchive = await _client.Repository.Content.GetArchive(id, ArchiveFormat.Zipball, commitId);
-            StoreByteArrayToZip(repoArchive);
-            //var compressedFileStream = new MemoryStream(repoArchive);
-            //using (var zipArchive = new ZipArchive(compressedFileStream, ZipArchiveMode.Read, false))
-            //{
 
-            //}
-
-
+            var repoDirectory = Path.Combine(downloadDirectory.FullName, $"{owner}-{name}-{commitId}");
+            
+            StoreByteArrayToZip(repoArchive, Directory.CreateDirectory(repoDirectory));
             Console.WriteLine("hello");
             Console.WriteLine(repoContent.Count);
         }
 
-
-        private void StoreByteArrayToZip(byte[] content)
+        private void StoreByteArrayToZip(byte[] content, DirectoryInfo downloadDirectory)
         {
             using (var compressedFileStream = new MemoryStream())
             //Create an archive and store the stream in memory.
@@ -59,67 +54,9 @@ namespace ReviewBee
                     originalFileStream.CopyTo(zipEntryStream);
                 }
 
-                zipArchive.ExtractToDirectory(@"C:/");
+                zipArchive.ExtractToDirectory(downloadDirectory.FullName);
                 //return new FileContentResult(compressedFileStream.ToArray(), "application/zip") { FileDownloadName = "Filename.zip" };
             }
         }
     }
-
-
-    public static class Zip
-    {
-        public static byte[] Decompress(byte[] zippedData)
-        {
-            byte[] decompressedData = null;
-            using (MemoryStream outputStream = new MemoryStream())
-            {
-                using (MemoryStream inputStream = new MemoryStream(zippedData))
-                {
-                    using (GZipStream zip = new GZipStream(inputStream, CompressionMode.Decompress))
-                    {
-                        zip.CopyTo(outputStream);
-                    }
-                }
-                decompressedData = outputStream.ToArray();
-            }
-
-            return decompressedData;
-        }
-
-        public static byte[] Compress(byte[] plainData)
-        {
-            byte[] compressesData = null;
-            using (MemoryStream outputStream = new MemoryStream())
-            {
-                using (GZipStream zip = new GZipStream(outputStream, CompressionMode.Compress))
-                {
-                    zip.Write(plainData, 0, plainData.Length);
-                }
-                //Dont get the MemoryStream data before the GZipStream is closed 
-                //since it doesn’t yet contain complete compressed data.
-                //GZipStream writes additional data including footer information when its been disposed
-                compressesData = outputStream.ToArray();
-            }
-
-            return compressesData;
-        }
-
-        public static void TestMethod1()
-        {
-            string text = "Hello World";
-
-            byte[] data = System.Text.Encoding.UTF8.GetBytes(text);
-
-            byte[] compressedData = Zip.Compress(data);
-
-            byte[] decompressedData = Zip.Decompress(compressedData);
-
-
-
-            string textAfterComDecompress = System.Text.Encoding.UTF8.GetString(decompressedData);
-        }
-
-    }
-
-
 }
